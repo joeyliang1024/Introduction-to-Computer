e57501469cf17b43bb3770d7087e095efed2cfac
joeyliang <95967774+joeyliang1024@users.noreply.github.com>
2022-02-18 13:37:12 +0800

    Add files via upload

---
+++ week1/A1_108201017.java
@@ -0,0 +1,15 @@
+/**
+Assignment 1
+Student numnber : 108201017
+Course : 2022-CE1004-B
+ */
+import java.util.Scanner;
+
+public class A1_108201017  {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+		Integer str1 = scanner.nextInt();
+        Integer str2 = scanner.nextInt();
+		System.out.println((str1+str2)+"\n"+(str1-str2));
+    }
+}
---
+++ week1/P1_108201017.java
@@ -0,0 +1,17 @@
+/**
+Practice 1
+Student numnber : 108201017
+Course : 2022-CE1004-B
+ */
+import java.util.Scanner;
+
+public class P1_108201017  {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input your name: ");
+		String str1 = scanner.nextLine();
+		System.out.print("Input your student ID: ");
+		String str2 = scanner.nextLine();
+		System.out.println("Hello World!!!\nI’m "+str1+" and my student ID is "+str2+".");
+    }
+}
