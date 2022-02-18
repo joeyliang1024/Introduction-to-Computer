3cdd5455190bf53301584468f7a662f59d31879e
joeyliang <95967774+joeyliang1024@users.noreply.github.com>
2022-02-18 13:38:52 +0800

    Update P1_108201017.java

--- week1/P1_108201017.java
+++ week1/P1_108201017.java
@@ -9,9 +9,9 @@ public class P1_108201017  {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Input your name: ");
-		String str1 = scanner.nextLine();
-		System.out.print("Input your student ID: ");
-		String str2 = scanner.nextLine();
-		System.out.println("Hello World!!!\nI’m "+str1+" and my student ID is "+str2+".");
+	String str1 = scanner.nextLine();
+	System.out.print("Input your student ID: ");
+	String str2 = scanner.nextLine();
+	System.out.println("Hello World!!!\nI’m "+str1+" and my student ID is "+str2+".");
     }
 }
