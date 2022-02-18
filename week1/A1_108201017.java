7c0fdc52f4d010199971a1de0adaee13b9d6bd50
joeyliang <95967774+joeyliang1024@users.noreply.github.com>
2022-02-18 14:17:55 +0800

    Update A1_108201017.java

--- week1/A1_108201017.java
+++ week1/A1_108201017.java
@@ -8,8 +8,8 @@ import java.util.Scanner;
 public class A1_108201017  {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-		Integer str1 = scanner.nextInt();
+	Integer str1 = scanner.nextInt();
         Integer str2 = scanner.nextInt();
-		System.out.println((str1+str2)+"\n"+(str1-str2));
+	System.out.println((str1+str2)+"\n"+(str1-str2));
     }
 }
